52deb5daaf6757f89c2980ae09569ee6959868a3
shivam goyal <2023aib1016@iitrpr.ac.in>
2024-08-23 00:30:04 +0530

    memory

--- src/ memoryManager.cpp
+++ src/ memoryManager.cpp
@@ -6,6 +6,9 @@ private:
     size_t physical_memory;
     size_t virtual_memory;
     size_t page_size;
+    int physical_pages=physical_memory/page_size;
+    int virtual_pages=virtual_memory/page_size;
+
 
 public:
     // Constructor
@@ -16,7 +19,14 @@ public:
     void displayMemory() const {
         cout << "Physical Memory: " << physical_memory << " bytes" << endl;
         cout << "Virtual Memory: " << virtual_memory << " bytes" << endl;
+         cout << "Physical page: " << physical_pages << " bytes" << endl;
+        cout << "Virtual page: " << virtual_pages << " bytes" << endl;
         cout << "Page Size: " << page_size << " bytes" << endl;
+    }
+    void addtask(string start_addres,int size){
+        int number_physical_page=size/page_size;
+        physical_pages-=number_physical_page;
+
     }
 };
 
@@ -24,6 +34,8 @@ int main() {
     // Create a MemoryManager object with example values
     MemoryManager manager(1000, 1000, 8);
     // Display memory information
+    manager.displayMemory();
+    manager.addtask("jasj",20);
     manager.displayMemory();
     return 0;
 }
