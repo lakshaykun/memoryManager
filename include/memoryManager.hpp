9a540d49350605b1fbdb7d4e5d17dc4ebaa9fa28
ShivamGoyal1507 <2023aib1016@iitrpr.ac.in>
2024-08-24 10:52:45 +0530

    memory

--- include/memoryManager.hpp
+++ include/memoryManager.hpp
@@ -1,8 +1,10 @@
-#include <bits/stdc++.h>
+#ifndef MEMORY_MANAGER
+#define MEMORY_MANAGER
+#include <iostream>
+#include <vector>
 using namespace std;
 
 class MemoryManager {
-
 private:
     size_t physical_memory;
     size_t virtual_memory;
@@ -10,6 +12,7 @@ private:
     size_t physical_pages_available;
     size_t virtual_pages;
     size_t physical_pages_allocated;
+    size_t physical_pages;
     size_t current_phy_page;
     std::vector<int> task;
     std::vector<bool> arr;
@@ -21,6 +24,7 @@ public:
           virtual_memory(vir),
           page_size(page),
           physical_pages_available(phy / page),
+          physical_pages(phy / page),
           virtual_pages(vir / page),
           physical_pages_allocated(0),
           current_phy_page(0),
@@ -28,38 +32,40 @@ public:
     {
         // Constructor body (if needed)
     }
-
    
-
-    long long request_memory(){
+    long long allocate_memory(){
         if(physical_pages_available>=1){
             physical_pages_allocated+=1;
             physical_pages_available-=1;
             physical_memory-=page_size;
-            arr[current_phy_page]=1;
-            current_phy_page+=1;
-            return current_phy_page-1;
+            
+            if(current_phy_page<physical_pages){
+                arr[current_phy_page]=1;
+                current_phy_page+=1;
+                return current_phy_page-1;
+            }
+            else{
+                for(size_t i=0;i<physical_pages;i++){
+                    if(arr[i]==0){
+                        arr[i]=1;
+                        return i;
+                    }
+                }
+            }
 
         }
         else{
             return -1;
         }
     }
-     void DisplayMemory(){
-        cout<<"PHYSICAL MEMORY : "<<physical_memory<<endl;
-        cout<<"VIRTUAL MEMORY : " <<virtual_memory<<endl;
-        cout<<"PHYSICAL PAGES ALLOCATED : "<<physical_pages_allocated<<endl;
+    void deallocatePage(int page){
+      physical_pages_allocated-=1;
+            physical_pages_available+=1;
+            physical_memory+=page_size;
+            arr[page]=0;
+            
 
     }
-
-
-
-    
-
-
-
-    // Other member functions can be defined here
 };
 
-
-
+#endif // MEMORY_MANAGER
--- src/ memoryManager.hpp
+++ src/ memoryManager.hpp
@@ -3,48 +3,11 @@ using namespace std;
 
 class MemoryManager {
 
-private:
-    size_t physical_memory;
-    size_t virtual_memory;
-    size_t page_size;
-    size_t physical_pages_available;
-    size_t virtual_pages;
-    size_t physical_pages_allocated;
-    size_t current_phy_page;
-    std::vector<int> task;
-    std::vector<bool> arr;
 
-public:
-    // Constructor
-    MemoryManager(size_t phy, size_t vir, size_t page)
-        : physical_memory(phy),
-          virtual_memory(vir),
-          page_size(page),
-          physical_pages_available(phy / page),
-          virtual_pages(vir / page),
-          physical_pages_allocated(0),
-          current_phy_page(0),
-          arr(phy / page, false) // Initialize vector arr with physical_pages elements set to false
-    {
-        // Constructor body (if needed)
-    }
 
    
 
-    long long request_memory(){
-        if(physical_pages_available>=1){
-            physical_pages_allocated+=1;
-            physical_pages_available-=1;
-            physical_memory-=page_size;
-            arr[current_phy_page]=1;
-            current_phy_page+=1;
-            return current_phy_page-1;
-
-        }
-        else{
-            return -1;
-        }
-    }
+    
      void DisplayMemory(){
         cout<<"PHYSICAL MEMORY : "<<physical_memory<<endl;
         cout<<"VIRTUAL MEMORY : " <<virtual_memory<<endl;
