41951d5bdc8d494a3006d60fd9f5780c1ffc198f
ShivamGoyal1507 <2023aib1016@iitrpr.ac.in>
2024-08-25 15:31:15 +0530

    final

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,21 @@
 #include <iostream>
 #include "../include/taskManager.hpp"
+#include "input_output.hpp"
+#include "../include/task.hpp"
+#include "../include/config.hpp"
+
 using namespace std;
 
-int main() {
+int main(){
+    string file;
+    cout<<"ENTER FILE PATH"<<endl;
+    cin>>file;
+    Trace_file_task(file);
+    
     // Create a TaskManager object
-    TaskManager taskManager;
+    
     // Add a task to the TaskManager
-    taskManager.addTask("T1", 0704004000, 1<<14);
-    // Display tasks
-    // taskManager.displayTasks();
-    // Remove a task from the TaskManager
-    // taskManager.removeTask("T1");
-    // Display tasks
-    taskManager.displayTasks();
-
-    taskManager.addTask("T2", 1704004000, 1<<12);
-    taskManager.addTask("T3", 3704004000, 1<<16);
-    taskManager.displayTasks();
-    // taskManager.displayMemoryManager();
+    taskManager.displayMemoryManager();
+    taskManager.Performance();
     return 0;
 }
